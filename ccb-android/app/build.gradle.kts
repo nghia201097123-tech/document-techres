daed85a4094a0a166be5ff98b21c054ce3e6ec2b
nghia201097123-tech <nghia201097123@gmail.com>
2026-01-09 22:50:38 +0700

    test

--- ccb-android/app/build.gradle.kts
+++ ccb-android/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
     buildTypes {
         debug {
             isMinifyEnabled = false
-            buildConfigField("String", "API_BASE_URL", "\"http://172.16.5.103:4000/api/tenant/\"")
-            buildConfigField("String", "API_POS_BASE_URL", "\"http://172.16.5.103:4000/api/pos/\"")
+            buildConfigField("String", "API_BASE_URL", "\"http://192.168.1.25:4000/api/tenant/\"")
+            buildConfigField("String", "API_POS_BASE_URL", "\"http://192.168.1.25:4000/api/pos/\"")
         }
         release {
             isMinifyEnabled = true
