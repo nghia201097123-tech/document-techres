7116a549ed3e8af44987cf1e63851b98f5df0ebd
nghia201097123-tech <nghia201097123@gmail.com>
2026-01-24 01:32:50 +0700

    test
    test

--- ccb-android/app/build.gradle.kts
+++ ccb-android/app/build.gradle.kts
@@ -26,11 +26,11 @@ android {
     buildTypes {
         debug {
             isMinifyEnabled = false
-            buildConfigField("String", "API_BASE_URL", "\"http://172.16.5.103:4000/api/tenant/\"")
-            buildConfigField("String", "API_POS_BASE_URL", "\"http://172.16.5.103:4000/api/pos/\"")
-            buildConfigField("String", "API_GATEWAY_URL", "\"http://172.16.5.103:4000/\"")
+            buildConfigField("String", "API_BASE_URL", "\"http://192.168.1.49:4000/api/tenant/\"")
+            buildConfigField("String", "API_POS_BASE_URL", "\"http://192.168.1.49:4000/api/pos/\"")
+            buildConfigField("String", "API_GATEWAY_URL", "\"http://192.168.1.49:4000/\"")
             // Socket-service runs on port 3007
-            buildConfigField("String", "SOCKET_URL", "\"http://172.16.5.103:3007\"")
+            buildConfigField("String", "SOCKET_URL", "\"http://192.168.1.49:3007\"")
         }
         release {
             isMinifyEnabled = true
