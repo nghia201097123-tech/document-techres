87fcc3bf07546d2b36300154a616b04bb723707f
nghia201097123-tech <nghia201097123@gmail.com>
2026-01-26 11:49:08 +0700

    tsst
    test

--- ccb-android/app/build.gradle.kts
+++ ccb-android/app/build.gradle.kts
@@ -26,19 +26,17 @@ android {
     buildTypes {
         debug {
             isMinifyEnabled = false
-            buildConfigField("String", "API_BASE_URL", "\"http://192.168.1.49:4000/api/tenant/\"")
-            buildConfigField("String", "API_POS_BASE_URL", "\"http://192.168.1.49:4000/api/pos/\"")
-            buildConfigField("String", "API_GATEWAY_URL", "\"http://192.168.1.49:4000/\"")
-            buildConfigField("String", "API_FOOD_BASE_URL", "\"http://192.168.1.49:3006/\"")
+            buildConfigField("String", "API_BASE_URL", "\"http://192.168.1.63:4000/api/tenant/\"")
+            buildConfigField("String", "API_POS_BASE_URL", "\"http://192.168.1.63:4000/api/pos/\"")
+            buildConfigField("String", "API_GATEWAY_URL", "\"http://192.168.1.63:4000/\"")
             // Socket-service runs on port 3007
-            buildConfigField("String", "SOCKET_URL", "\"http://192.168.1.49:3007\"")
+            buildConfigField("String", "SOCKET_URL", "\"http://192.168.1.63:3007\"")
         }
         release {
             isMinifyEnabled = true
             buildConfigField("String", "API_BASE_URL", "\"https://api.techres.vn/api/tenant/\"")
             buildConfigField("String", "API_POS_BASE_URL", "\"https://api.techres.vn/api/pos/\"")
             buildConfigField("String", "API_GATEWAY_URL", "\"https://api.techres.vn/\"")
-            buildConfigField("String", "API_FOOD_BASE_URL", "\"https://food.techres.vn/\"")
             // Socket-service production URL
             buildConfigField("String", "SOCKET_URL", "\"https://socket.techres.vn\"")
             proguardFiles(
