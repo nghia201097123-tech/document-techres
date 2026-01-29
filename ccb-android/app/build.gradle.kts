de3e9e135177a782995e2fbb5b5e3c16c04fec3a
nghia201097123-tech <nghia201097123@gmail.com>
2026-01-29 10:49:18 +0700

    sdsd
    sdsd

--- ccb-android/app/build.gradle.kts
+++ ccb-android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         debug {
             isMinifyEnabled = false
             // APISIX Gateway URL - điều hướng request qua header x-svc-id
-            buildConfigField("String", "APISIX_GATEWAY_URL", "\"https://beta.api.gateway.overate-vntech.com/\"")
+            buildConfigField("String", "APISIX_GATEWAY_URL", "\"http://localhost:4000/\"")
             // Service port IDs (dùng làm x-svc-id header)
             buildConfigField("String", "SVC_ID_OAUTH", "\"1506\"")
             buildConfigField("String", "SVC_ID_DASHBOARD", "\"1503\"")
